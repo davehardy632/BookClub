01537bb9a52682282dbe3e01c29b5c7dea1a22f3
JennicaStiehl <jennica.stiehl@gmail.com>
2019-03-21 07:44:24 -0600

    worked on fixing a book show page test.
    Co-authored-by: David Hardy <42919604+davehardy632@users.noreply.github.com>

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe 'As a Visitor' do
 
   describe 'When I visit a books show page' do
-    @terry = Author.create(name:"Terry")
-    @book = @terry.books.create(title:"Houses",pages:300, year:1984)
     xit 'i see a link to add a new review for this book' do
-      visit book_path(@book)
+      terry = Author.create(name:"Terry")
+      book = terry.books.create(title:"Houses",pages:300, year:1984)
+      visit book_path(book)
 
-      expect(page).to have_content(@book.title)
+      expect(page).to have_content(book.title)
 
-      visit new_book_review_path(@book.id)
+      visit new_book_review_path(book.id)
 
       fill_in :review_title, with: 'great'
       fill_in :review_user, with: 'tony'
@@ -19,7 +19,7 @@ RSpec.describe 'As a Visitor' do
 
       click_on "Create Review" #When the form is submitted, I should return to that book's
 
-      expect(current_path).to eq(book_path(@book))
+      expect(current_path).to eq(book_path(book))
 
       expect(page).to have_content('had to read it twice, it was so good')
 
@@ -27,19 +27,19 @@ RSpec.describe 'As a Visitor' do
   end
 
   describe "When I visit a book's show page," do
-    @gaga = Author.create(name:"Lady Gaga")
-    @ted = Author.create(name:"Ted Bundy")
-    @book_1 = @gaga.books.create(title: "Book 1", pages: 300, year: 1973, cover_image: "www.book.com")
-    @book_2 = @ted.books.create(title: "Book 2", pages: 340, year: 1988, cover_image: "www.book.com")
-    @book_3 = @gaga.books.create(title: "Book 3", pages: 200, year: 1971, cover_image: "www.book.com")
-  
     it "shows me one book title, author, pages, pub year, large image" do
-      visit book_path(@book_1)
+      gaga = Author.create(name:"Lady Gaga")
+      ted = Author.create(name:"Ted Bundy")
+      book_1 = gaga.books.create(title: "Book 1", pages: 300, year: 1973, cover_image: "www.book.com")
+      book_2 = ted.books.create(title: "Book 2", pages: 340, year: 1988, cover_image: "www.book.com")
+      book_3 = gaga.books.create(title: "Book 3", pages: 200, year: 1971, cover_image: "www.book.com")
+      visit book_path(book_1.id)
 
-      expect(page).to have_content(@book_1.title)
-      expect(page).to have_content(@book_1.pages)
-      expect(page).to have_content(@book_1.year)
-      expect(page).to have_content(@gaga.name)
+
+      expect(page).to have_content(book_1.title)
+      expect(page).to have_content(book_1.pages)
+      expect(page).to have_content(book_1.year)
+      expect(page).to have_content(gaga.name)
     end
   end
 end
@@ -47,4 +47,3 @@ end
 # Each review will have a title and user, a numeric rating
 # from 1 to 5, and text for the review itself, and all content
 # must be present for each review.
-
