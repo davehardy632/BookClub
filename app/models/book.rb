edc220f1524d0e30c5f641960f1bb71605f756b1
David Hardy <davehardy632@gmail.com>
2019-03-24 10:53:29 -0600

    Completed card 19, statistics bar in book show page

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,10 +7,23 @@ class Book < ApplicationRecord
   validates_presence_of :year
 
   def average_rating
-    self.reviews.average(:rating)
+    reviews.average(:rating)
   end
 
   def total_reviews
     self.reviews.count(:rating)
   end
+
+  def top_three_reviews
+    reviews.order(rating: :desc).limit(3)
+  end
 end
+# Dog.order(age: :desc)	.order allows us to retrieve records ordered by specified attributes. :desc specifies descending order.
+# Dog.limit(2)
+
+# As a Visitor,
+# When I visit a book's show page,
+# I see an area on the page for statistics about reviews:
+# - the top three reviews for this book (title, rating and user only)
+# - the bottom three reviews for this book  (title, rating and user only)
+# - the overall average rating of all reviews for this book
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,19 @@
 class Review < ApplicationRecord
   belongs_to :book
 
-  # validates_uniqueness_of :user
+  # def reviews_by_rating(book, direction)
+  #    book.reviews.order(rating: direction).limit(3)
+  #  end
+  #
+  # def avg_rating
+  #   reviews.average(:rating)
+  # end
+
+  def self.top_three
+    order(rating: :desc).limit(3)
+  end
+
+  def self.bottom_three
+    order(rating: :asc).limit(3)
+  end
 end
